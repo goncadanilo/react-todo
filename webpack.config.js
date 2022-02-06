c592567e236e18565e5c5d349f4eae911412137b
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-02-06 10:28:51 -0300

    fix: make the babel-loader accept .tsx and .ts files

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(j|t)sx$/,
+        test: /\.(j|t)(sx|s)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
